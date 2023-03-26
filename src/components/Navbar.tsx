494963eff6a2e797f95809afd2774df0d8a61213
Ian <minecraftindo0@gmail.com>
2023-03-26 12:37:15 +0700

    Performance animation go to section on nav item clicked

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,25 +10,48 @@ export const Navbar = () => {
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
+        window.addEventListener("scroll", handleScroll, {passive: true});
 
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        return () => {
+            let url = window.location.href.split("/");
+            let target = url[url.length - 1].toLowerCase();
+            let element = document.getElementById(target);
+            element && element.scrollIntoView({behavior: "smooth", block: "start"});
         };
     }, []);
 
 
     function openNav() {
-        setOpen(!open)
+        setOpen(!open);
+    }
+
+    function goToSection(section: string, e: { preventDefault: () => void }) {
+        let sec = document.getElementById(section);
+        e.preventDefault();  // Stop Page Reloading
+        sec && sec.scrollIntoView({ behavior: "smooth", block: "start" });
+        window.history.pushState(section, section, `/${section}`);
+    }
+
+    function goToTop(event: { preventDefault: () => void }) {
+        window.scrollTo({top: 0, behavior: "smooth"});
+        event.preventDefault();
+        window.history.pushState("", "", `/`);
     }
 
     return (
         <header className="sticky top-0 z-50">
-            <div className={`w-full text-gray-700 bg-white ${scrollPosition > 0 ? 'shadow-md' : ''}`}>
+            <div className={`w-full text-gray-700 bg-white ${scrollPosition > 0 ? "shadow-md" : ""}`}>
                 <div
                     className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
                     <div className="p-4 flex flex-row items-center justify-between">
-                        <a href="#"
+                        <a href="/"
+                            onClick={goToTop}
                            className="flex flex-row text-lg font-semibold tracking-widest text-gray-900 rounded-lg focus:outline-none focus:shadow-outline">
                             <span className="pl-2">TheManusia</span>
                         </a>
@@ -38,19 +61,26 @@ export const Navbar = () => {
                             <Bars3Icon className={`w-6 h-6 ${open ? "hidden" : "inline"}`}/>
                         </button>
                     </div>
-                    <nav className={`${open ? "flex" : "hidden"} flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row`}>
+                    <nav
+                        className={`${open ? "flex" : "hidden"} flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row`}>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">About</a>
+                           onClick={event => goToSection("about", event)}
+                           href="/">About</a>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">Skills</a>
+                           onClick={event => goToSection("skills", event)}
+                           href="/">Skills</a>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">Educations</a>
+                           onClick={event => goToSection("educations", event)}
+                           href="/">Educations</a>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">Experiences</a>
+                           onClick={event => goToSection("experiences", event)}
+                           href="/">Experiences</a>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">Projects</a>
+                           onClick={event => goToSection("projects", event)}
+                           href="/">Projects</a>
                         <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg md:mt-0 hover:text-gray-900 hover:bg-gray-200"
-                           href="#">Contact</a>
+                           onClick={event => goToSection("contact", event)}
+                           href="/">Contact</a>
                     </nav>
                 </div>
             </div>
